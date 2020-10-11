9758b3c61254db391a34af54954aecf759139e4e
Rylan Wassem <dryland28@gmail.com>
2020-10-11 17:07:23 -0400

    Updates

---
+++ precache-manifest.fe327aedcc07594455318f508a6026a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3438c9c3549eacef59ad33045db2d9c",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/index.html"
+  },
+  {
+    "revision": "9bb086879b2f4c62d5fe",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/static/css/main.17dbcf05.chunk.css"
+  },
+  {
+    "revision": "92777c93a4d9f39e25bf",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/static/js/2.97dcab7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/static/js/2.97dcab7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb086879b2f4c62d5fe",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/static/js/main.6214ad50.chunk.js"
+  },
+  {
+    "revision": "84e58d58532821715477",
+    "url": "/thinkful-ei-shark/wassem-rylan-bookmarks-app/static/js/runtime-main.069a2e42.js"
+  }
+]);
